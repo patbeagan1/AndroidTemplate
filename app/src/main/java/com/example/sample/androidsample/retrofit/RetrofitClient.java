8fd96eae4ff223b8ebfd383474c4426bd8f1a120
Patrick Beagan <pbeagan1@gmail.com>
2018-07-25 00:44:26 -0400

    Fixing some tests

--- app/src/main/java/com/example/sample/androidsample/retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/sample/androidsample/retrofit/RetrofitClient.java
@@ -1,36 +1,36 @@
-    package com.example.sample.androidsample.retrofit;
+package com.example.sample.androidsample.retrofit;
 
-    import com.example.sample.androidsample.retrofit.github.GitHubAPI;
-    import com.example.sample.androidsample.retrofit.pixabay.PixabayAPI;
+import com.example.sample.androidsample.retrofit.github.GitHubAPI;
+import com.example.sample.androidsample.retrofit.pixabay.PixabayAPI;
 
-    import retrofit2.Retrofit;
-    import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-    public class RetrofitClient {
+public class RetrofitClient {
 
-        private static GitHubAPI gitHubAPI;
-        private static PixabayAPI pixabayAPI;
+    private static GitHubAPI gitHubAPI;
+    private static PixabayAPI pixabayAPI;
 
-        private static Retrofit getClient(String baseUrl) {
-            return new Retrofit.Builder()
-                    .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
-
-        public static GitHubAPI getGithubService() {
-            if (gitHubAPI == null) {
-                // Getting a client for https://api.github.com
-                gitHubAPI = getClient("https://api.github.com").create(GitHubAPI.class);
-            }
-            return gitHubAPI;
-        }
-
-        public static PixabayAPI getPixabayService() {
-            if (pixabayAPI == null) {
-                // Getting a client for https://pixabay.com
-                pixabayAPI = getClient("https://pbay.com").create(PixabayAPI.class);
-            }
-            return pixabayAPI;
-        }
+    private static Retrofit getClient(String baseUrl) {
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
+
+    public static GitHubAPI getGithubService() {
+        if (gitHubAPI == null) {
+            // Getting a client for https://api.github.com
+            gitHubAPI = getClient("https://api.github.com").create(GitHubAPI.class);
+        }
+        return gitHubAPI;
+    }
+
+    public static PixabayAPI getPixabayService() {
+        if (pixabayAPI == null) {
+            // Getting a client for https://pixabay.com
+            pixabayAPI = getClient("https://pbay.com").create(PixabayAPI.class);
+        }
+        return pixabayAPI;
+    }
+}
